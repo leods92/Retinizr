3c19f50d6745c6d89f06b24111dabde2ee6bc9d8
Leonardo D. Schlossmacher <e@leods92.com>
2013-03-26 00:41:18 -0300

    Updated to 0.1.1, added support to gravatars and google static maps scaling.

--- README.md
+++ README.md
@@ -1,48 +1,148 @@
+**Attention:** the API has changed in version `0.1.1`.
+Now, you are required to send image-scaling related options wrapped in an object called `images`, e.g. `Retinizr({ images: { ... } })`.
+The default images' css class is now `hires-image`. You can keep your mark-up and just call `Retinizr({ images: { css_class: "hires_img"} })` to mimic `0.1.0` version behavior.
+
 # Retinizr
 A jQuery-less script that replaces images with ones optmized for high density devices (as those with Apple Retina).
 
 ## Usage
-Include `Retinizr.min.js` in the `head` or in the end of the `body` (prefferable) section of your HTML document.
-Add CSS class `hires_img` (or another one, this is configurable) to all `img` elements you want to provide a high resolution version.
+Include `Retinizr.min.js` in the `head` or at the end of the `body` (prefferable) section of your HTML document.
+Add CSS class `hires_img` (or the one you configured) to all `img` elements you want to provide a high resolution version.
 Afterwards, cakk Retinizr with `Retinizr()`.
 
 For example:
 ```html
-<script src="Retinizr.min.js"></script>
-<script>
-  Retinizr();
-</script>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <script src="Retinizr.min.js"></script>
+  </head>
+  <body>
+    <img src="" alt="" />
+    <script>
+      Retinizr();
+    </script>
+  </body>
 ```
+### Google Static Maps API (v2)
+Google provides an execellent API to retrieve images of maps. As the images are static you get what you get and scalling them will cause distortion. Hopefully, Google accepts a parameter call `scale` which generate another image with the *exactly* same viewport size and position but denser.
+Retinizr deals with that automatically for you.
 
-## Settings
-Settings can be set by sending an object as the first parameter in the `Retinizr()` call.
+This is disabled by default though.
+In order to enable it, when calling `Retinizr()` set the option `google_static_maps` to `true` or an object (`{...}`) containing the options you want.
+For example: `Retinizr({ google_static_maps: true })`
 
-The options are:
-* `css_class`: The CSS class your upgradeable images have.
-* `source_suffix`: The text before high resolution images' extension. E.g.: if set to `"@2x"` then `logo.png` becomes `logo@2x.png`. Note: regular expression characters must be escaped.
+More information about this functionality is availble in the *Options* section bellow.
+
+### Gravatar
+If you want to *automagically* change gravatars diplayed on your page to a denser version, Retinizr can do that for you.
+
+This is disabled by default though.
+In order to enable it, when calling `Retinizr()` set the option `gravatars` to `true` or an object (`{...}`) containing the options you want.
+For example: `Retinizr({ gravatars: true })`
+
+More information about this functionality is availble in the *Options* section bellow.
+
+## Options
+Options can be set by sending an object as the first parameter in the `Retinizr()` call.
+
+The global options are:
 * `min_pixel_ratio`: The density ratio of the screen you are targeting to. Apple Retina devices have 2.0 ratio and some Android devices 1.5. 1.0 is the standard we're all used to. You might want to check this <a href="http://en.wikipedia.org/wiki/List_of_displays_by_pixel_density" target="_blank">list of screen densities</a>, pay attention to the "CSS pixel ratio" columns.
+* `images`: Enables or disables images scaling.
+* `google_static_maps`: Enables or disables Google Static Maps scaling.
+* `gravatars`: Enables or disbles Gravatars scaling.
+
+Defaults:
+* `min_pixel_ratio`: `1.5`
+* `images`: `true`
+* `google_static_maps`: `false`
+* `gravatars`: `false`
 
 Here's an example:
 ```html
 <script src="Retinizr.min.js"></script>
 <script>
   Retinizr({
-    css_class: 'superultramega_hires_img',
-    source_suffix: '_hires',
-    min_pixel_ratio: 2.0
+    min_pixel_ratio: 2.0,
+    google_static_maps: true
   });
 </script>
 ```
 
-And here are the defaults that will be used in case you don't override them:
-* `css_class`: `"hires_img"`
-* `source_suffix`: `"@2x"`
-* `min_pixel_ratio`: `1.5`
+As generating images appropriated for each density would be time-consuming we suggest you to use 1.5 in `min_pixel_ratio` while providing an image twice as dense as the original one what would cover devices from 1.5 to 2.0 screen densities.
 
-As generating images appropriated for each density is time-consuming we suggest you to use 1.5 in `min_pixel_ratio` while providing an image for 2.0 devices only.
+### Images
+To use these you should set an object with the options you want to the `images` key of the main options object.
+
+* `css_class`: The CSS class your scalable images have.
+* `source_suffix`: The text before high resolution images' extension. E.g.: if set to `"@2x"` then `logo.png` becomes `logo@2x.png`. Note: regular expression characters must be escaped.
+
+Defaults:
+* `css_class`: `highres-image`
+* `source_suffix`: `"@2x"`
+
+Here's an example:
+```html
+<script src="Retinizr.min.js"></script>
+<script>
+  Retinizr({
+    images: {
+      css_class: "my-much-cooler-custom-css-class",
+      source_suffix: "_highres"
+    }
+  });
+</script>
+```
+
+### Google Static Maps
+To use these you should set an object with the options you want to the `google_static_maps` key of the main options object.
+
+* `css_class`: The CSS class your scalable map images have.
+* `scale`: The number by which the size of the original map will be multiplied. Google accepts 1, 2, 4 (the latter available for business customers only).
+
+Defaults:
+* `css_class`: `highres-map`
+* `scale`: `2`
+
+Here's an example:
+```html
+<script src="Retinizr.min.js"></script>
+<script>
+  Retinizr({
+    google_static_maps: {
+      css_class: "my-much-cooler-custom-css-class",
+      scale: 4 // "4" is excessive, you probably don't need such a dense image and is only availble in the paid version of the API.
+    }
+  });
+</script>
+```
+
+### Gravatars
+To use these you should set an object with the options you want to the `gravatars` key of the main options object.
+
+* `css_class`: The CSS class your scalable gravatar images have.
+* `scale`: The number by which the size of the original gravatar will be multiplied.
+
+Defaults:
+* `css_class`: `highres-gravatar`
+* `scale`: `2`
+
+Here's an example:
+```html
+<script src="Retinizr.min.js"></script>
+<script>
+  Retinizr({
+    gravatar: {
+      css_class: "my-much-cooler-custom-css-class",
+      scale: 2
+    }
+  });
+</script>
+```
 
 ## License
-Copyright (c) 2012, Leonardo D. Schlossmacher.
+Copyright (c) 2013, Leonardo D. Schlossmacher.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
--- Retinizr.js
+++ Retinizr.js
@@ -1,13 +1,13 @@
-/*!
- * Retinizr - version 0.1.0
- * Copyright (c) 2012, Leonardo D. Schlossmacher (leods92.com).
+/*!preserve
+ * Retinizr - version 0.1.1
+ * Copyright (c) 2013, Leonardo D. Schlossmacher (leods92.com).
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-;(function(window){
+;(function(window, document){
 
   // If the browser doesn't set the pixel density it's not being run in a high density display device and so we won't proceed.
   // Since high density screens are only available in modern devices we'll rely on forEach method of the Array object.
@@ -15,21 +15,87 @@
     return;
   }
 
-  var document = window.document
-    , defaults = {
-        css_class: 'hires_img' // The CSS class your upgradeable images have.
-      , source_suffix: '@2x' // The text before high resolution images' extension. E.g.: logo.png => logo@2x.png. Note: regular expression characters must be escaped.
-      , min_pixel_ratio: 1.5 // Apple Retina devices have 2.0 ratio but opted the default to be 1.5 as that's the ratio of some Android devices displays.
+  //
+  // Options
+  //
+  var defaults = {
+        min_pixel_ratio: 1.5 // Apple Retina devices have 2.0 ratio but I opted the default to be 1.5 as that's the ratio of some Android devices displays.
+      , images: {
+          css_class: "hires-image" // The CSS class your upgradeable images have.
+        , source_suffix: "@2x" // The text before high resolution images' extension. E.g.: logo.png => logo@2x.png. Note: regular expression characters must be escaped.
+      }
+      , google_static_maps: {
+          css_class: "hires-map"
+        , scale: 2 // Google also provides scale=4 for Business API users
+      }
+      , gravatars: {
+          css_class: "hires-gravatar"
+        , scale: 2 // Be careful not to exceed Gravatar's limit that is 2048px. Note that even though the image provided will be in the requested size, user's source image might not be that big and so the quality may be compromised.
+      }
     }
+    , options = {}
   ;
 
-  function getImagesList(css_class) {
+  function setOptions(_options) {
+    _options = _options || {};
+
+    var pickOption = function(custom, _default) {
+      return typeof custom !== "undefined" ? custom : _default;
+    };
+
+    for (var prop in defaults) {
+      if (typeof defaults[prop] === "object") {
+        // In case a boolean was set, we'll set all options as default.
+        if (typeof _options[prop] !== "object") {
+          options[prop] = defaults[prop];
+          continue;
+        }
+
+        options[prop] = {};
+
+        for (var childProp in defaults[prop]) {
+          options[prop][childProp] = pickOption(_options[prop][childProp], defaults[prop][childProp]);
+        }
+        continue;
+      }
+
+      options[prop] = pickOption(_options[prop], defaults[prop]);
+    }
+  }
+
+
+  //
+  // DOM searching
+  //
+  function getList(css_class) {
     return document.getElementsByClassName(css_class);
+  }
+
+
+  //
+  // Images
+  //
+  function scaleImages() {
+    var images = getImagesList();
+    scaleItems("scaleImage", images);
+  }
+
+  function getImagesList() {
+    return getList(options.images.css_class);
+  }
+
+  function scaleImage(img) {
+    var new_src = img.src.replace(/^(.*)(\.[a-z]{3,4})$/, '$1' + options.images.source_suffix + '$2');
+
+    loadImage(new_src, function() {
+      // Retinizr will first load the image then replace for a better user experience.
+      img.src = new_src;
+    });
   }
 
   function loadImage(src, callback) {
     var xhr = new XMLHttpRequest();
-    
+
     xhr.open("GET", src, true);
     xhr.onreadystatechange = function() {
       if (xhr.readyState === 4 && xhr.status === 200) {
@@ -40,47 +106,85 @@
     xhr.send();
   }
 
-  function upgradeImage(img, suffix) {
-    var new_src = img.src.replace(/^(.*)(\.[a-z]{3,4})$/, '$1' + suffix + '$2');
 
-    loadImage(new_src, function() {
-      // Retinizr will first load the image then replace for a better user experience.
-      img.src = new_src;
-      console.log('callback called');
-    });
+  //
+  // Google Static Maps
+  //
+  function scaleGoogleStaticMaps() {
+    var maps = getGoogleStaticMapsList();
+    scaleItems("scaleGoogleStaticMap", maps);
   }
 
-  function upgradeImages(settings) {
-    var images;
+  function getGoogleStaticMapsList() {
+    return getList(options.google_static_maps.css_class);
+  }
 
-    settings = typeof settings === 'object' ? settings : {};
+  function scaleGoogleStaticMap(map) {
+    map.src = map.src.replace("scale=1", "scale=2");
+  }
 
-    for (var prop in defaults) {
-      if (typeof settings[prop] === 'undefined') {
-        settings[prop] = defaults[prop];
-      }
-    }
 
-    images = getImagesList(settings.css_class);
+  //
+  // Gravatars
+  //
+  function scaleGravatars() {
+    var gravatars = getGravatarsList();
+    scaleItems("scaleGravatar", gravatars);
+  }
 
-    Array.prototype.forEach.call(images, function(img) {
-      var is_hires;
+  function getGravatarsList() {
+    return getList(options.gravatars.css_class);
+  }
 
-      if (!(img instanceof HTMLImageElement)) {
-        throw "Element has class '" + settings.css_class + "' but is not an img element.";
-      }
+  function scaleGravatar(gravatar) {
+    gravatar.src = gravatar.src.replace(/(size|s)=([0-9]+)/, "$1" + parseInt("$2", 10)*options.gravatar.scale);
+  }
 
-      is_hires = (new RegExp(settings.source_suffix + '\\.[a-z]{3,4}')).test(img.src);
 
+  //
+  // Helpers
+  //
+  function scaleItems(scalingFunction, items) {
+    if (!items) return; // returns if null or undefined
+
+    Array.prototype.forEach.call(items, function(item) {
+      checkHTMLElement(item);
       // It'll only replace the image if it's not already the high resolution version.
-      // Only on devices with at least the settings.min_pixel_ratio high resolution images will be displayed.
-      if (!is_hires && window.devicePixelRatio >= settings.min_pixel_ratio) {
-        upgradeImage(img, settings.source_suffix);
-      }
+      if (!deviceRequiresRetinazation() || elIsRetinized(item)) return;
+
+      this[scalingFunction](item);
+      setElAsRetinized(item);
     });
   }
 
-  // Exposes Retinizr
-  window.Retinizr = upgradeImages;
+  function checkHTMLElement(el) {
+    if (!(el instanceof HTMLImageElement)) {
+      throw "Element has proper class was supposed to be retinized but is not an img element.";
+    }
+  }
 
-})(window);
+  function elIsRetinized(el) {
+    return !!el['data-retinized'];
+  }
+
+  function setElAsRetinized(el) {
+    el['data-retinized'] = true;
+  }
+
+  function deviceRequiresRetinazation() {
+    return window.devicePixelRatio >= options.min_pixel_ratio;
+  }
+
+
+  //
+  // Exposes Retinizr
+  //
+  window.Retinizr = function(_options) {
+    setOptions(_options);
+
+    if (_options.scaleImages !== false) scaleImages();
+    if (_options.google_static_maps) scaleGoogleStaticMaps();
+    if (_options.gravatars) scaleGravatars();
+  };
+
+})(window, window.document);
